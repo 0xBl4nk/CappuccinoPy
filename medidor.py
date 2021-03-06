c4b2bde538ae0840faf4fe2cb5f8f66b9e747dbc
Arthur Ottoni Ribeiro <61481946+ArthurHydr@users.noreply.github.com>
2021-03-06 18:23:15 -0300

    modify#1

---
+++ medidor.py
@@ -0,0 +1,21 @@
+
+QUANTIDADE_COPO = float(input("Digite quantos copos utilizará: "))
+TAMANHO_COPO = float(input("Digite o tamanho em ml do seu copo: "))
+
+def medirColheres():
+    primeiraRegra = 14 * QUANTIDADE_COPO
+    quantidadeColher = primeiraRegra / 7
+    print(f'{quantidadeColher} colheres de sopa por copo!')
+
+def medirAguaELeite():
+    quantAgua = TAMANHO_COPO / 7
+    quantLeite = TAMANHO_COPO - quantAgua
+    print(f'{quantAgua:.2f} ml de água por copo!')
+    print(f'{quantLeite:.2f} ml de leite por copo!')
+
+
+def resultado():
+    medirColheres()
+    medirAguaELeite()
+
+resultado()
