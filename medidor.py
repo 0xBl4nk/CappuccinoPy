973b3b406255bb398da51c23b19bafc3c8a0f692
Arthur Ottoni Ribeiro <61481946+ArthurHydr@users.noreply.github.com>
2021-03-07 12:58:22 -0300

    bigm

--- medidor.py
+++ medidor.py
@@ -3,39 +3,49 @@ import os
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-clear_screen()
+def exit_screen():
+    os.system('pause' if os.name == 'nt' else 'echo "Aperte ENTER para fechar..."\n read enter')
+    clear_screen()
 
-QUANTIDADE_COPO = float(input("Digite a quantidade de copos: "))
-TAMANHO_COPO = float(input("Digite o tamanho em ml dos copos: "))
+def get_spoons(cup_qnt) -> float:
+    return (14 * cup_qnt) / 7
 
-clear_screen()
+def get_water(cup_qnt, cup_size) -> float:
+    if cup_qnt > 1:
+        return (float(14.285714285714) * cup_size / 100) * cup_qnt
+    else:
+        return cup_size / 7
 
-def medirColheres():
-    return (14 * QUANTIDADE_COPO) / 7
+def get_milk(cup_qnt, cup_size) -> float:
+    if cup_qnt > 1:
+        return (float(85.714285714286) * cup_size / 100) * cup_qnt
+    else:
+        return cup_size - get_water(cup_qnt, cup_size)
 
-def medirAgua():
-    return (14.285714285714 * TAMANHO_COPO / 100) * QUANTIDADE_COPO if  QUANTIDADE_COPO > 1 else TAMANHO_COPO / 7
+def show_recipe(cup_qnt, cup_size) -> str:
+    return (
+        'Igredientes: \n'
+        f'[+] {get_spoons(cup_qnt):.0f} colheres de pó de capuccino.\n'
+        f'[+] {get_water(cup_qnt, cup_size):.2f}ml de água.\n'
+        f'[+] {get_milk(cup_qnt, cup_size):.2f}ml de leite.\n')
 
-def medirLeite():
-    return (85.714285714286 * TAMANHO_COPO / 100) * QUANTIDADE_COPO if QUANTIDADE_COPO > 1 else TAMANHO_COPO - medirAgua()
+def show_steps(cup_qnt, cup_size) -> str:
+    return (
+        'Modo de Preparo: \n'
+        f'[*] Esquente {get_water(cup_qnt, cup_size):.2f}ml.\n'
+        f'[*] Adicione {get_spoons(cup_qnt):.0f} colheres de pó de capuccino. (não deixe ferver)\n'
+        f'[*] Pegue essa mistura e coloque na geladeira, até que esfrie.\n'
+        f'[*] Após a mistura esfriar, coloque no liquidificador e bata junto com {get_milk(cup_qnt, cup_size):.2f}ml de leite.\n'
+        f'[*] E pronto, seu capuccino está feito receita by: Rafaelvis Presley!.'
+    )
 
-def ingredientes():
-    print("\nIngredientes: \n")
-    print(f"[+] {medirColheres()} colheres de pó de capuccino.")
-    print(f"[+] {medirAgua():.2f} ml de água.")
-    print(f"[+] {medirLeite():.2f} ml de leite.")
+def main():
+    clear_screen()
+    cup_qnt = int(input('Digite a quantidade de copos: '))
+    cup_size = float(input('Digite o tamanho do copo: '))
+    clear_screen()
+    print(show_recipe(cup_qnt, cup_size))
+    print(show_steps(cup_qnt, cup_size), "\n")
 
-
-def preparo():
-    print("\nModo de preparo: \n")
-    print(f"[+] Esquente {medirAgua():.2f} ml de água e adicione {medirColheres()} colheres de pó de capuccino (NÃO DEIXA FERVER NÃO HEIM!)")
-    print("[+] Pegue a mistura e coloque na geladeira até que esfrie.")
-    print(f"[+] Após esfriar coloque a mistura no liquidificador junto com {medirLeite():.2f} ml de leite e bata.")
-    print("[+] Está pronto seu capuccino gelado da Rafaelvis Presley!\n")
-
-
-ingredientes()
-preparo()
-
-input("Aperte qualquer tecla para sair...")
-clear_screen()
+main()
+exit_screen()
