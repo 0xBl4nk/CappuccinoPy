10d180ed344dff48209b5238c3291758fcf65b72
Arthur Ottoni Ribeiro <61481946+ArthurHydr@users.noreply.github.com>
2021-03-06 22:52:40 -0300

    organização

--- medidor.py
+++ medidor.py
@@ -10,17 +10,21 @@ def medirAgua():
 def medirLeite():
     return (85.714285714286 * TAMANHO_COPO / 100) * QUANTIDADE_COPO if QUANTIDADE_COPO > 1 else TAMANHO_COPO - medirAgua()
 
-print("\nIngredientes: \n")
-print(f"* {medirColheres()} colheres de pó de capuccino.")
-print(f"* {medirAgua():.2f} ml de água.")
-print(f"* {medirLeite():.2f} ml de leite.")
+def ingredientes():
+    print("\nIngredientes: \n")
+    print(f"* {medirColheres()} colheres de pó de capuccino.")
+    print(f"* {medirAgua():.2f} ml de água.")
+    print(f"* {medirLeite():.2f} ml de leite.")
 
 
+def preparo():
+    print("\nModo de preparo: \n")
+    print(f"* Esquente {medirAgua():.2f} ml de água e adicione {medirColheres()} de pó de capuccino (NÃO DEIXA FERVER NÃO HEIM!)")
+    print("* Pegue a mistura e coloque na geladeira até que esfrie.")
+    print(f"* Após esfriar coloque a mistura no liquidificador junto com {medirLeite():.2f} e bata.")
+    print("* Está pronto seu capuccino gelado da Rafaelvis Presley!\n")
 
-print("\nModo de preparo: \n")
-print(f"* Esquente {medirAgua():.2f} ml de água e adicione {medirColheres()} de pó de capuccino (NÃO DEIXA FERVER NÃO HEIM!)")
-print("* Pegue a mistura e coloque na geladeira até que esfrie.")
-print(f"* Após esfriar coloque a mistura no liquidificador junto com {medirLeite():.2f} e bata.")
-print("* Está pronto seu capuccino gelado da Rafaelvis Presley!\n")
+    input("Aperte qualquer tecla para fechar...")
 
-input("Aperte qualquer tecla para fechar...")
+ingredientes()
+preparo()
